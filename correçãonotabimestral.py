05a7001847bf734fcf00e2bc92d0ccc8c0827724
mariaeduardasastre <pereirasastre@gmail.com>
2025-08-19 08:16:07 -0300

    correção.exercício.notabimestral

---
+++ correçãonotabimestral.py
@@ -0,0 +1,4 @@
+nota1=float(input("Digite a primeira nota"))
+nota2=float(input("digite a segunda nota"))
+notafalta=60 - (nota1*2+nota2*2)
+print("Você precisa tira" , ((notafalta/2))/3 , "nos próximos 2 bimestres")

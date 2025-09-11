435b36cfabcb170ec17b3ca656eefd4da19788a7
mariaeduardasastre <pereirasastre@gmail.com>
2025-09-11 10:36:36 -0300

    Add files via upload

---
+++ ListaExercícios.LaçosRepetição.9.py
@@ -0,0 +1,9 @@
+num = int(input("Digite um número: "))
+divisores: 0
+for i in range(1, num+1):
+    if num % 1 == 0:
+     divisores == 1
+    if divisores == 2:
+       print(num, "é primo.")
+    else:
+       print(num, "não é primo.")

ee9743aa9ce62d6e5a7b1623bbadd57009164edb
mariaeduardasastre <pereirasastre@gmail.com>
2025-09-11 10:03:51 -0300

    Add files via upload

---
+++ ListaExercícios.LaçosRepetiçãoWhile8.py
@@ -0,0 +1,11 @@
+num = int(input("Digite um número: "))
+divisores=0
+i = 1
+while i <= 1:
+    if num % 1 ==0:
+     divisores += 1
+    1 += 1
+if divisores == 2: 
+   print(num, "é primo.")
+else:
+   print(num, "Não é primo.")

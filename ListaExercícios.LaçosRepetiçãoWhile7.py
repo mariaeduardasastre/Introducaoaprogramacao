ba70c1857cc295682eb6d8ccd41ea3dabd1fcbc1
mariaeduardasastre <pereirasastre@gmail.com>
2025-09-11 09:59:13 -0300

    Add files via upload

---
+++ ListaExercícios.LaçosRepetiçãoWhile7.py
@@ -0,0 +1,8 @@
+thanos = 1.50
+aranha = 1.10
+anos = 0
+while aranha <= thanos:
+    thanos += 0.02
+    aranha += 0.03
+    anos += 1
+print("Serão necessários" , anos, "anos para o homem aranha ser maior que o thanos.")
